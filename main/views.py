c0c5981484ce19bddc69dea06a3a4dcd2dcf182c
Plucky <root@localhost.localdomain>
2025-01-28 16:25:18 +0300

    progress Tue Jan 28 04:25:18 PM EAT 2025

--- main/views.py
+++ main/views.py
@@ -1,11 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required #ensures only logged in users can visit home
+from .models import Listing
+from .forms import ListingForm
+from users.forms import LocationForm
 
 # Create your views here.
 def main_view(request):
     return render(request, "views/main.html", {"name": "Automax"})
 @login_required #checking if user is logged in
 def home_view(request):
-    return render(request, "views/home.html")
+    listings = Listing.objects.all()
+    context = {
+        'listings': listings,
+    }
+    return render(request, "views/home.html", context)
+
+@login_required
+def list_view(request):
+    if request.method == 'POST':
+        pass
+    elif request.method == 'GET':
+        listing_form = ListingForm()
+        location_form = LocationForm()
+    return render(request, 'views/list.html', {'listing_form': listing_form, 'location_form': location_form})
     
