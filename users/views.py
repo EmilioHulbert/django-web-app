27aa4dbfb99281e4b6f35fc3b1f23e4e343553dc
Remac <remac@github.com>
2025-01-30 10:08:55 +0300

    progress

--- users/views.py
+++ users/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.views import View
 from django.utils.decorators import  method_decorator
 from .forms import UserForm, LocationForm, ProfileForm
-from main.models import Listing
+from main.models import Listing, LikedListing
 # from http.client import HTTPResponse
 # from django.http import HttpResponse
 
@@ -73,15 +73,17 @@ class ProfileView(View):
     def get(self, request):
         # Listing.objects.get()
         user_listings = Listing.objects.filter(seller=request.user.profile)
+        user_liked_listings = LikedListing.objects.filter(profile=request.user.profile).all()
         user_form = UserForm(instance=request.user)
         profile_form = ProfileForm(instance=request.user.profile)
         location_form = LocationForm(instance=request.user.profile.location)
         return render(request, 'views/profile.html', {'user_form': user_form,
          'profile_form': profile_form,
-         'location_form': location_form, 'user_listings': user_listings})
+         'location_form': location_form, 'user_listings': user_listings, 'user_liked_listings': user_liked_listings, })
 
     def post(self, request):
             user_listings = Listing.objects.filter(seller=request.user.profile)
+            user_liked_listings = LikedListing.objects.filter(profile=request.user.profile).all()
             user_form = UserForm(request.POST, instance=request.user)
             profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
             location_form = LocationForm(request.POST, instance=request.user.profile.location)
@@ -94,4 +96,4 @@ class ProfileView(View):
                 messages.error(request, 'Error Updating Profile!')
             return render(request, 'views/profile.html', {'user_form': user_form,
          'profile_form': profile_form,
-         'location_form': location_form, 'user_listings': user_listings})
+         'location_form': location_form, 'user_listings': user_listings, 'user_liked_listings': user_liked_listings, })
